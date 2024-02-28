6d2b928d34811a45a2a944b42c2dc031c994cb32
Gourab <roygourab005@gmail.com>
2024-02-28 20:07:43 +0530

    initial setup for the project to shorten a long URL

---
+++ models/url.model.js
@@ -0,0 +1,19 @@
+ import mongoose, {Schema} from "mongoose";
+
+ const urlSchema = new Schema ({
+    shortId:{
+        type: String,
+        required: true
+    },
+    redirectUrl:{
+        type: String,
+        required: true,
+    }, 
+    visitHistory:[
+        {
+            time : Number
+        }
+    ]
+ }, {timestamps: true});
+
+  export const URL = mongoose.model("URL", urlSchema)
